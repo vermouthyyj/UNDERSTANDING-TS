e942370bb11c043f8d6de68f0f64d0146a15e546
vermouthyyj <yanyujun666@gmail.com>
2022-05-27 22:03:16 +0800

    the never type

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-function add(n1, n2) {
-    return n1 + n2;
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
-var number1 = 5;
-var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+var combineAges = combine(30, 26);
+console.log(combineAges);
+var combineNames = combine('Max', 'Anna');
+console.log(combineNames);
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
--- app.ts
+++ app.ts
@@ -1,9 +1,24 @@
-function add(n1, n2) { 
-    return n1 + n2;
+function combine(input1: number | string, input2: number | string) { 
+    let result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    }
+    else { 
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
 
-const number1 = 5;
-const number2 = 2.8;
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) { 
+    const result = n1 + n2;
+    cb(result)
+}
 
-const result = add(number1, number2);
-console.log(result);
+const combineAges = combine(30, 26);
+console.log(combineAges);
+
+const combineNames = combine('Max', 'Anna');
+console.log(combineNames);
+addAndHandle(10, 20, (result) => { 
+    console.log(result);
+})
